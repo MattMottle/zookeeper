7d88b98bb1046a88888f1c6196f08f9e9d2e3cd6
Matt Mottle <mattmottle1@gmail.com>
2023-08-10 16:51:11 -0700

    combined class tests

--- src/zookeeper/AnimalTest.java
+++ src/zookeeper/AnimalTest.java
@@ -1,9 +1,10 @@
 package zookeeper;
 
-public class GorillaTest {
+public class AnimalTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Gorilla g1 = new Gorilla();
 		
 		g1.displayEnergy();
@@ -14,6 +15,20 @@ public class GorillaTest {
 		g1.eatBananas();
 		g1.climb();
 		g1.displayEnergy();
+		
+		BatosaurausRex b1 = new BatosaurausRex();
+		
+		b1.displayEnergy();
+		b1.attackTown();
+		b1.attackTown();
+		b1.attackTown();
+		b1.eatHumans();
+		b1.eatHumans();
+		b1.fly();
+		b1.fly();
+		b1.displayEnergy();
+		
+		
 	}
 
 }
